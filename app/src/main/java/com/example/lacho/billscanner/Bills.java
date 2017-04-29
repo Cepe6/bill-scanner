8151aa0987e832cbc7c01250d88ed1c5dcb897fa
lmladenov <lachomladenov@gmail.com>
2017-04-29 22:07:23 +0300

    Adding test kinvey query

---
+++ app/src/main/java/com/example/lacho/billscanner/Bills.java
@@ -0,0 +1,37 @@
+package com.example.lacho.billscanner;
+
+import com.google.api.client.json.GenericJson;
+import com.google.api.client.util.Key;
+import com.kinvey.java.model.KinveyMetaData;
+
+import org.json.JSONObject;
+
+/**
+ * Created by lacho on 4/29/17.
+ */
+
+public class Bills extends GenericJson { // For Serialization
+
+    @Key("_id")
+    private String id;
+    @Key
+    private JSONObject bill;
+    @Key
+    private double totalPrice;
+    @Key("_kmd")
+    private KinveyMetaData meta; // Kinvey metadata, OPTIONAL
+    @Key("_acl")
+    private KinveyMetaData.AccessControlList acl; //Kinvey access control, OPTIONAL
+
+
+    public Bills() {
+    }  //GenericJson classes must have a public empty constructor
+
+    public void setBill(JSONObject bill) {
+        this.bill = bill;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
--- app/src/main/java/com/example/lacho/billscanner/MainActivity.java
+++ app/src/main/java/com/example/lacho/billscanner/MainActivity.java
@@ -10,8 +10,13 @@ import android.view.View;
 import android.widget.Button;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
+import com.kinvey.android.AsyncAppData;
 import com.kinvey.android.Client;
 import com.kinvey.android.callback.KinveyPingCallback;
+import com.kinvey.java.core.KinveyClientCallback;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,24 +24,53 @@ public class MainActivity extends AppCompatActivity {
     private TessBaseAPI mTess;
     String datapath = "";
     Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         //WIP -> create client and then check connectivity
-        final Client mKinveyClient = new Client.Builder("", "", this.getApplicationContext()).build();
+        final Client mKinveyClient = new Client.Builder(getString(R.string.app_key),
+                getString(R.string.app_secret),
+                this.getApplicationContext()).build();
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         mKinveyClient.ping(new KinveyPingCallback() {
             public void onFailure(Throwable t) {
                 Log.e("", "Kinvey Ping Failed", t);
             }
             public void onSuccess(Boolean b) {
-                Log.d("", "Kinvey Ping Success");
+                Log.i("", "Kinvey Ping Success");
             }
         });
-        //END OF WIP
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //END OF WIP
+        Bills bills = new Bills();
+        bills.setTotalPrice(4.4D);
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject = new JSONObject("{\"phonetype\":\"N95\",\"cat\":\"WP\"}");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        bills.setBill(jsonObject);
+
+        AsyncAppData<Bills> myevents = mKinveyClient.appData("bills", Bills.class);
+        myevents.save(bills, new KinveyClientCallback<Bills>() {
+            @Override
+            public void onFailure(Throwable e) {
+                Log.e("TAG", "failed to save event data", e);
+            }
+
+            @Override
+            public void onSuccess(Bills b) {
+                Log.d("TAG", "saved data for entity " + b.toString());
+            }
+        });
+        mKinveyClient.query();
 
         button = (Button) findViewById(R.id.button);
 
